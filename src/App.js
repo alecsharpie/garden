e1cbdb269098fab079e3c4135849d0b41b528f5d
alec sharp <alecsharpie@gmail.com>
2023-10-25 01:49:12 +1100

    add multi backend touch to support mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GardenContainer from "./components/GardenContainer";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { DndProvider } from "react-dnd";
+import { DndProvider } from 'react-dnd-multi-backend'
+import { HTML5toTouch } from 'rdndmb-html5-to-touch' // or any other pipeline
+
 
 function App() {
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider options={HTML5toTouch}>
         <GardenContainer />
       </DndProvider>
     </div>
